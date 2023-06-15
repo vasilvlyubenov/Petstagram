bc2db687918173dbc79c72f8a7a0f425404351a5
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-06-15 20:38:01 +0300

    added db configuration

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
 const PORT = 3000;
 
 const app = express();
+
+dbConnect()
+    .then(() => console.log('Connected to DB succesfully'))
+    .catch (err => {
+        console.log('DB ERROR:', err);
+    });
 
 expressConfig(app);
 handlebarsConfig(app);
@@ -13,4 +20,4 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.listen(PORT, () => { console.log(`Server is listening on port ${PORT}...`) });
+app.listen(PORT, () => { console.log(`Server is listening on port ${PORT}...`); });
