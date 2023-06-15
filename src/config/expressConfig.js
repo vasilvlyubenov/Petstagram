3b9714dffca1fa41e9bc4b3de3f677ecf781d8a4
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-06-15 22:53:21 +0300

    added coockie parser, authentication and dynamic navigation

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const {auth} = require('../middleware/authMiddleware');
 
 function expressConfig(app) {
     app.use(express.static(path.resolve(__dirname, '../')));
     app.use(express.urlencoded({ extended: false}));
+    app.use(cookieParser());
+    app.use(auth);
 }
 
 module.exports = expressConfig; 
